b18b7f5de32954e77058a8e99ee18205af4580a0
MarnitzDev <malanmarnitz@gmail.com>
2025-08-06 13:18:28 +0200

    axios fix

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,4 +1,10 @@
 import axios from 'axios';
+
+// Set axios as a global window property if you want
 window.axios = axios;
 
+// Set the baseURL explicitly using environment variable or fallback to current origin
+window.axios.defaults.baseURL = import.meta.env.VITE_APP_URL || window.location.origin;
+
+// Set common headers
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
