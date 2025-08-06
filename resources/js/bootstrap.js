a054af552b2855384a458e5e6c3624ef741bcd17
MarnitzDev <malanmarnitz@gmail.com>
2025-08-06 13:22:16 +0200

    axios fix

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,10 +1,9 @@
-import axios from 'axios';
+import axios from 'axios'
 
-// Set axios as a global window property if you want
-window.axios = axios;
+window.axios = axios
 
-// Set the baseURL explicitly using environment variable or fallback to current origin
-window.axios.defaults.baseURL = import.meta.env.VITE_APP_URL || window.location.origin;
+// ✅ This forces HTTPS if the page is served over HTTPS
+window.axios.defaults.baseURL = window.location.origin
 
-// Set common headers
-window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+// Required headers
+window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest'
