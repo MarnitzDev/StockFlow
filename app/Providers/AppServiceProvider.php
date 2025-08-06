67508268af9847cbcf1f203ddc060982297777d6
MarnitzDev <malanmarnitz@gmail.com>
2025-08-06 13:29:01 +0200

    revert

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -20,10 +20,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        if (app()->environment('production')) {
-            URL::forceScheme('https');
-        }
-
         Vite::prefetch(concurrency: 3);
     }
 }
