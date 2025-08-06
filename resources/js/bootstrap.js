eaf6de431a92848750b5dc78c6dac39f22b9f93c
MarnitzDev <malanmarnitz@gmail.com>
2025-08-06 21:57:55 +0200

    axious xsrf-token

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,9 +1,16 @@
+
 import axios from 'axios'
 
 window.axios = axios
 
 // Required for Laravel
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest'
+
+// Enable sending credentials with cross-domain requests
+window.axios.defaults.withCredentials = true;
+
+// Enable automatic XSRF-TOKEN handling
+window.axios.defaults.withXSRFToken = true;
 
 // Get CSRF from meta tag
 const token = document.head.querySelector('meta[name="csrf-token"]')
