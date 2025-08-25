de0c9eedf375a70ee85ab2e022b8098036c8e4ec
MarnitzDev <malanmarnitz@gmail.com>
2025-08-25 10:17:13 +0200

    RefreshAndSeedDatabase

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -12,7 +12,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
-        $schedule->command('db:refresh-and-seed')->dailyAt('10:05');
+        $schedule->command('db:refresh-and-seed')->dailyAt();
     }
 
     /**
